9cb31781b92d1a17b68f428e6fe170448dd42c64
ZealIT-Digital <zealitdigital@gmail.com>
2024-04-11 16:34:31 +0530

    update_1104_1634

--- webapp/controller/Initial_01.controller.js
+++ webapp/controller/Initial_01.controller.js
@@ -830,7 +830,8 @@ sap.ui.define(
                   if (oData.results.length != 0) {
                     var DrawingURl = oData.results[0];
                     var sSource = DrawingURl.Data01;
-                    window.open("http://" + sSource);
+                    window.open('http://' + sSource);
+                    // window.location.href('file://' + sSource);
                   }
                 } catch (e) {
                   alert(e.message);
